61d3ac09b357ac704d7331ef8f5f3b23bb498346
StefanoSacchet <sacchet13@gmail.com>
2024-11-13 10:18:56 +0100

    Fixed hotel dataset

--- Phase 2 - Information Gathering/Code Libraries/hotel_dataset/script.py
+++ Phase 2 - Information Gathering/Code Libraries/hotel_dataset/script.py
@@ -1,3 +1,5 @@
+import ssl
+
 import certifi
 import geopy.geocoders
 import pandas as pd
@@ -19,6 +21,11 @@ data.drop("FAX", axis=1, inplace=True)
 data.drop("Partita IVA", axis=1, inplace=True)
 data.drop("Numero unita", axis=1, inplace=True)
 data.drop("Posti letto", axis=1, inplace=True)
+data.drop("Tipologia unita", axis=1, inplace=True)
+data.drop("Prezzo massimo consumazione pasto", axis=1, inplace=True)
+data.drop("Prezzo massimo letto aggiunto", axis=1, inplace=True)
+data.drop("Tipologia alberghiera", axis=1, inplace=True)
+data.drop("Provincia", axis=1, inplace=True)
 
 # Translate rows to english
 data["Categoria"] = data["Categoria"].replace(
@@ -36,27 +43,12 @@ data["Categoria"] = data["Categoria"].replace(
         "nessuna-stella": "no-stars",
     }
 )
-data["Tipologia alberghiera"] = data["Tipologia alberghiera"].replace(
-    {
-        "albergo": "hotel",
-        "rta": "tourist hotel residence",
-        "garni": "hotel",
-    }
-)
 data["Tipologia servizio"] = data["Tipologia servizio"].replace(
     {
         "pensione-completa": "full-board",
         "mezza-pensione": "half-board",
         "solo-pernottamento": "room-only",
         "prima-colazione": "bed-and-breakfast",
-    }
-)
-data["Tipologia unita"] = data["Tipologia unita"].replace(
-    {
-        "camera-senza-bagno": "room-without-bathroom",
-        "camera-con-bagno": "room-with-bathroom",
-        "appartamento": "apartment",
-        "bicamera-con-bagno": "two-room-with-bathroom",
     }
 )
 
@@ -117,6 +109,43 @@ column_translation = {
 
 # Rename columns
 data.rename(columns=column_translation, inplace=True)
+
+ctx = ssl.create_default_context(cafile=certifi.where())
+geopy.geocoders.options.default_ssl_context = ctx
+geolocator = Nominatim(user_agent="crag_locator")
+
+# Add coordinates
+structure_positions = {}
+total_rows = len(data)
+progress_interval = total_rows // 10  # 10% intervals
+completed_count = 0
+for i, municipality in enumerate(data["Municipality"].unique()):
+    try:
+        location = geolocator.geocode(f"{municipality.lower()}, Trento", timeout=3)
+        if location:
+            structure_positions[municipality] = (location.latitude, location.longitude)
+        else:
+            print(f"Coordinates not found for {municipality}")
+    except Exception as e:
+        print(f"Error retrieving coordinates for {municipality}: {e}")
+
+    completed_count += 1
+    if completed_count % progress_interval == 0:
+        print(f"Progress: {completed_count / total_rows * 100:.0f}%")
+
+coord_df = pd.DataFrame.from_dict(
+    structure_positions, orient="index", columns=["latitude", "longitude"]
+)
+coord_df.reset_index(inplace=True)
+coord_df.rename(columns={"index": "Municipality"}, inplace=True)
+
+# output_path = "only_coordinates.csv"
+# coord_df.to_csv(output_path, index=False, sep=";")
+
+data = data.merge(coord_df, on=["Municipality"], how="left")
+
+# Add unique ID
+data.insert(0, "id", ["hotel" + str(i) for i in range(len(data))])
 
 output_path = "hotels.csv"
 data.to_csv(output_path, index=False, sep=";")
